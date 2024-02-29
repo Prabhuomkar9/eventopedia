6beddc403fc6b2fa3dc29d908e00f9ddfa0a91e9
Prabhuomkar9 <prabhuomkar9@gmail.com>
2024-02-29 21:50:23 +0530

    Added user API test routes

--- src/pages/api/seed.ts
+++ src/pages/api/seed.ts
@@ -2,9 +2,5 @@ import prisma from "@/server/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  prisma.user.deleteMany().then(() => {
-    console.log("Deleted all users");
-  });
-  res.status(200).json({});
-  prisma.$disconnect();
+  return res.status(200).json({ message: "Hello World" });
 }
