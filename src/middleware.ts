3e16c483bdbc4241510d6455ec70639b89b0fe6b
Prabhuomkar9 <prabhuomkar9@gmail.com>
2024-03-04 21:20:59 +0530

    .gitignore update

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,4 +3,4 @@ export { default } from 'next-auth/middleware'
 
 // Applies next-auth only to matching routes - can be regex
 //Ref : https://nextjs.org/docs/pages/building-your-application/routing/middleware#matcher
-export const config = { matcher: ["/dashboard/(.*)"] }
+export const config = { matcher: ["/dashboard/(.*)", "/profile"] }
