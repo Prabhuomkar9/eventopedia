85bdc18b54c32037af847984407d2c0d86c89cab
Prabhuomkar9 <prabhuomkar9@gmail.com>
2024-03-06 22:09:41 +0530

    Whats happening

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -8,7 +8,22 @@ import { Button } from "~/components/ui/button";
 import Link from "next/link";
 
 const Profile: NextPage = () => {
-  return <div>Profile</div>;
+  const { data: session, status, update } = useSession();
+
+  if (!session) return <NotFound />;
+
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      Profile
+      {session && session.user.role !== "USER" && (
+        <Link href={`/dashboard/${session.user.role.toLowerCase()}`}>
+          <Button>Dashboard</Button>
+        </Link>
+      )}
+      <ProfileIcon />
+      <SignOut />
+    </div>
+  );
 };
 
 export default Profile;
